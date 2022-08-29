082a137c404461f554ba3a530c18f0f49a81abe4
Ryan Zayne <phoenixastro97@gmail.com>
2022-08-29 21:45:29 +0100

    update

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const navToggle = document.querySelector(".nav-bar");
 const menuToggle = document.querySelector(".hamburger");
 
 menuToggle.addEventListener("click", () => {
-    navToggle.classList.toggle("menu-toggle"); 
-    menuToggle.classList.toggle("close");
-})
+  navToggle.classList.toggle("menu-toggle");
+  menuToggle.classList.toggle("close");
+});
