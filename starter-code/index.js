08cc8ffce68b38518988b8576915004d8d8c9616
Ryan Zayne <phoenixastro97@gmail.com>
2022-08-29 14:46:57 +0100

    Change repo structure

---
+++ starter-code/index.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector(".nav-bar");
+const menuToggle = document.querySelector(".hamburger");
+
+menuToggle.addEventListener("click", () => {
+    navToggle.classList.toggle("menu-toggle"); 
+    menuToggle.classList.toggle("close");
+})
