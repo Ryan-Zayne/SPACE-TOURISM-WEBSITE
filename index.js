f71fce266dd38e8fac5654ead3a6d7cd6178f364
Ryan Zayne <phoenixastro97@gmail.com>
2022-08-31 22:17:53 +0100

    Last nitpick update

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const navToggle = document.querySelector(".nav-bar");
-const menuToggle = document.querySelector(".hamburger");
+const navToggle = document.querySelector(".nav-bar"),
+  menuToggle = document.querySelector(".hamburger"),
+  mainContent = document.querySelector(".main-container");
 
 menuToggle.addEventListener("click", () => {
   navToggle.classList.toggle("menu-toggle");
   menuToggle.classList.toggle("close");
+});
+
+// This block is to make the hamburger disappear if user clicks outside the navigation block
+mainContent.addEventListener("click", () => {
+  if (navToggle.classList.contains("menu-toggle") && menuToggle.classList.contains("close")) {
+    navToggle.classList.remove("menu-toggle");
+    menuToggle.classList.remove("close");
+  }
 });
