9fe8f1d882f6dc8f4db3db9013ccfc3762d03a9d
Ryan Zayne <phoenixastro97@gmail.com>
2022-09-24 00:36:10 +0100

    .

--- navigation.js
+++ navigation.js
@@ -1,18 +1,9 @@
 const navBar = document.querySelector(".nav-bar");
 const hamburgerToggle = document.querySelector(".hamburger");
-const mainContent = document.querySelector(".main-container");
 
 hamburgerToggle.addEventListener("click", () => {
   navBar.classList.toggle("menu-toggle");
   hamburgerToggle.classList.toggle("close");
-});
-
-// This block is to make the hamburger disappear if user clicks outside the navigation block
-mainContent.addEventListener("click", () => {
-  if (navBar.classList.contains("menu-toggle") && hamburgerToggle.classList.contains("close")) {
-    navBar.classList.remove("menu-toggle");
-    hamburgerToggle.classList.remove("close");
-  }
 });
 
 
