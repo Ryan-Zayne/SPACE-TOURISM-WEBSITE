2cf5b5db9c0647a955a31723f8f8db1fa817425d
Ryan Zayne <phoenixastro97@gmail.com>
2022-10-16 00:27:08 +0100

    .

--- fiveserver.config.js
+++
@@ -1,6 +0,0 @@
-// fiveserver.config.js
-module.exports = {
-    host: '0.0.0.0', // default: '0.0.0.0' (could also be 'localhost' for example)
-    remoteLogs: 'magenta', // true | false | Color
-    useLocalIp: true, // optional: opens browser with your local IP
-  }
