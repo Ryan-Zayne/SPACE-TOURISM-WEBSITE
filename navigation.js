3858bf60f419f9cdea061b55877ef043e1bcb777
Ryan Zayne <phoenixastro97@gmail.com>
2022-10-16 01:42:32 +0100

    few linting changes

--- navigation.js
+++ navigation.js
@@ -1,7 +1,7 @@
-const navBar = document.querySelector(".nav-bar");
-const hamburgerToggle = document.querySelector(".hamburger");
+const navBar = document.querySelector('.nav-bar');
+const hamburgerToggle = document.querySelector('.hamburger');
 
-hamburgerToggle.addEventListener("click", () => {
-  navBar.classList.toggle("menu-toggle");
-  hamburgerToggle.classList.toggle("close");
+hamburgerToggle.addEventListener('click', () => {
+	navBar.classList.toggle('menu-toggle');
+	hamburgerToggle.classList.toggle('close');
 });
--- tab.js
+++ tab.js
@@ -1,33 +1,28 @@
 import * as navigationJs from './navigation.js';
 
-
 const tabPanel = document.querySelectorAll('.tab-span');
 
-tabPanel.forEach( (tab) => tab.addEventListener("click", changeTabPanel));
+tabPanel.forEach((tab) => tab.addEventListener('click', changeTabPanel));
 
-
-
-/*This function changes the tab panel and the image when a tab is clicked.*/
-
+/* This function changes the tab panel and the image when a tab is clicked. */
 function changeTabPanel(e) {
-    const targetTab = e.target;     /* This refers back to the particular element that      
-                                    triggers the event, which is "click" in this case */
+	const targetTab = e.target; /* This refers back to the particular element that
+                            	triggers the event, which is "click" in this case */
+	const targetArticle = targetTab.getAttribute('aria-controls');
+	const targetImage = targetTab.getAttribute('data-image');
+	const tabContainer = targetTab.parentNode;
+	const secondaryContainer = tabContainer.parentNode;
+	const primaryContainer = secondaryContainer.previousElementSibling;
 
-    const targetArticle = targetTab.getAttribute("aria-controls");
-    const targetImage = targetTab.getAttribute("data-image")
-    const tabContainer = targetTab.parentNode;
-    const secondaryContainer = tabContainer.parentNode;
-    const primaryContainer = secondaryContainer.previousElementSibling;
+	secondaryContainer
+		.querySelectorAll('.tab-article')
+		.forEach((article) => article.setAttribute('hidden', true));
 
-    secondaryContainer.querySelectorAll('.tab-article')
-                       .forEach((article) => article.setAttribute('hidden', true));
-    
-    secondaryContainer.querySelector(`#${targetArticle}`).removeAttribute('hidden');
+	secondaryContainer.querySelector(`#${targetArticle}`).removeAttribute('hidden');
 
+	primaryContainer
+		.querySelectorAll('.image-holder')
+		.forEach((image) => image.setAttribute('hidden', true));
 
-    primaryContainer.querySelectorAll('.image-holder')
-                     .forEach((image) => image.setAttribute('hidden', true));
-    
-    primaryContainer.querySelector(`#${targetImage}`).removeAttribute('hidden');
-
-}   
+	primaryContainer.querySelector(`#${targetImage}`).removeAttribute('hidden');
+}
